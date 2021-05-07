fffd4c67ec2b2f74c1c9677ccea81837d19ddaf7
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-05-07 18:40:00 +0200

    first name error validation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
     Hello this is form!
+    <span class="message" v-if="firstNameError"
+      >Error: First Name required to short</span
+    >
+
     <form>
-      <fieldset name="contact"></fieldset>
+      <fieldset name="contact">
+        <input type="text" id="first-name" v-model="firstName" />
+      </fieldset>
     </form>
   </div>
 </template>
 
 <script>
+//  Error: First Name required to short
+
 export default {
   name: "Form",
+  data() {
+    return {
+      firstName: "",
+    };
+  },
+  computed: {
+    firstNameError() {
+      return this.firstName.length < 2 ? true : false;
+    },
+  },
 };
 </script>
--- tests/unit/Form.spec.js
+++ tests/unit/Form.spec.js
@@ -1,9 +1,9 @@
 import { configFactory, wrapperFactory } from "./utils.js";
 import Form from "@/components/Form.vue";
-let wrapper;
+let wrapper, config;
 
 beforeEach(() => {
-  const config = {
+  config = {
     propsData: {},
   };
   wrapper = wrapperFactory(Form, configFactory(config));
@@ -17,12 +17,43 @@ describe("Form.vue", () => {
   it("is a Vvue instance", () => {
     expect(wrapper.isVueInstance).toBeTruthy();
   });
+
   it("has a form element", () => {
     const form = wrapper.find("form");
     expect(form.exists()).toBe(true);
   });
+
   it("has a fieldset named contact", () => {
     const contactFieldset = wrapper.find("fieldset[name='contact']");
     expect(contactFieldset.exists()).toBe(true);
+  });
+
+  it("has contact field with first name input", () => {
+    const firstNameInput = wrapper.find(
+      "fieldset[name='contact'] #first-name[type='text']"
+    );
+    expect(firstNameInput.exists()).toBe(true);
+  });
+
+  it("Error displays if first name text length is < 2", async () => {
+    config = {
+      data() {
+        return {
+          firstName: "",
+        };
+      },
+    };
+
+    wrapper = wrapperFactory(Form, configFactory(config));
+
+    const firstNameInput = wrapper.find(
+      "fieldset[name='contact'] #first-name[type='text']"
+    );
+
+    await firstNameInput.setValue("x");
+
+    const message = wrapper.find(".message");
+
+    expect(message.text()).toBe("Error: First Name required to short");
   });
 });
